7078033e4491e56c46f15ac09e4173ac61344ce2
Michael <mioncgp@gmail.com>
2020-05-22 15:12:59 +0400

    first commit

---
+++ 1 Section/four pillars of OOP.js
@@ -0,0 +1,21 @@
+`Those four pillars are: 
+ - Encapsulation.
+ - Abstraction.
+ - Inheritance.
+ - Polymorphism.`
+
+ `In OOP we combine a group of related variables and functions into a unit. We call that unit an object or
+ In OPP we group related variables and functions that operate on them into objects.
+ This is an instance of ENCAPSULATION.`
+
+
+ `In OOP we can hide some of the properties and methods from the outside. It will give certain benefits.
+ Firstly it is going to make the interface of those objects simpler. If changes are made in an object
+ it is not going to impact the rest of the code base in other words "Reduce the impact of change".
+ This is Abstraction.`
+
+
+ `Inheritance is a mechanism that allows to eliminate redundunt code. Some objects share properties and methids.`
+
+ `Polymorphism means many forms. It is a technique that allows to get rid of long "if" and "else" statements.
+ We can implement a method in each of the objects and it the method will behave diffrently depending on the object we are referencing. `
---
+++ 1 Section/what is OOP.js
@@ -0,0 +1,1 @@
+`It is a programming paradigm centered around objects rather than functions. It is a style of programming`
